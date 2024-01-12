2bffe6d87d3946a3ae1cd10198ec3b2fedd34583
Md.Sazid <115614150+mdsazid123@users.noreply.github.com>
2024-01-12 22:05:13 +0530

    class create is a c++

---
+++ user.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+class Tex{// tex is a class define ;
+public:// public is is member of dta ;
+int a=10;
+float b=2.5;
+};
+int main()
+{
+    
+    int  a=20;
+    cout<<"enter a value";
+    cout<<"enter a value";
+
+}
+
